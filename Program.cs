fe2cae2bf3aa165d5f374fcff24a29e3ad5b6408
IrinaVolodko <8232734@gmail.com>
2023-07-12 13:41:24 +0300

    First task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("Input five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int tenThousand = (number / 10000) % 10;
+int thousand = (number / 1000) % 10;
+int hundreds = (number / 100) % 10;
+int dozits = (number / 10) % 10;
+int units = number % 10;
+if (units == tenThousand && dozits == thousand)
+{
+    Console.WriteLine($"Yes, {number} is palindrome");
+}
+else { Console.WriteLine($"No, {number} isn't palindrome"); }
+
+
